ecd45c07c700b0c8bae95fa0eb0f6e48c52859f8
PhamHoa <phambichhoa2410@gmail.com>
2021-04-26 21:38:05 +0700

    Full header

--- html/js/dropDownHeader.js
+++ html/js/dropDownHeader.js
@@ -5,8 +5,24 @@ function myFunction() {
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+function userDropDown() {
+    document.getElementById("userDrop").classList.toggle("show")
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        let dropdowns = document.getElementsByClassName("dropdownContent");
+        let i;
         for (i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show')) {
