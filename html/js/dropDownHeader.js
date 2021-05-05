a80776f9185caee2ebb20f82940ca7d1acc74ca3
PhamHoa <phambichhoa2410@gmail.com>
2021-05-05 11:31:02 +0700

    Fix cart

--- html/js/dropDownHeader.js
+++ html/js/dropDownHeader.js
@@ -1,10 +1,26 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
 
-// Close the dropdown menu if the user clicks outside of it
+function userDropDown() {
+    document.getElementById("userDrop").classList.toggle("show")
+};
+
 window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
+        let dropdowns = document.getElementsByClassName("dropdownContent");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+};
+function categoryDropDown() {
+    document.getElementById("categoryDrop").classList.toggle("show")
+};
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropCategoryBtn')) {
         let dropdowns = document.getElementsByClassName("dropdown-content");
         let i;
         for (i = 0; i < dropdowns.length; i++) {
@@ -14,20 +30,4 @@ window.onclick = function(event) {
             }
         }
     }
-}
-
-function userDropDown() {
-    document.getElementById("userDrop").classList.toggle("show")
-}
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        let dropdowns = document.getElementsByClassName("dropdownContent");
-        let i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
+};
